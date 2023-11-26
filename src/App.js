43617f5ecea863af1deb96cba517025fd5e8fc83
Alienatedem <bloxinckoi@mail.ru>
2023-11-26 15:41:45 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Блохина Эмиля Евгеньевича, test CI/CD";
 
-  // console.log('This is App ');
+  console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
