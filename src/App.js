c82fd882623c10027825632b6b8b7e68dbc2be2c
Alienatedem <bloxinckoi@mail.ru>
2023-11-28 18:55:44 +0300

    feat: add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Блохина Эмиля Евгеньевича, test CI/CD is complited " ;
+  const greeting = "Это тестовый проект Блохина Эмиля Евгеньевича, test CI/CD is complited feat " ;
 
   return (
     <div className="App">
