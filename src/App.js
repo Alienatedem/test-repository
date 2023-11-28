a5d48bf839ea784df57b20e4e72946707c77fa86
Alienatedem <bloxinckoi@mail.ru>
2023-11-28 18:45:08 +0300

    feat: Added semantic-release

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Блохина Эмиля Евгеньевича, test CI/CD is complited " ;
 
-  // console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
